482651fdef81ca581cf93c54373d1d2abfb42f05
jaydevelopsstuff <70743392+jaydevelopsstuff@users.noreply.github.com>
2026-05-26 16:06:42 -0700

    Final polish for example

--- examples/tauri-app/src-tauri/src/lib.rs
+++ examples/tauri-app/src-tauri/src/lib.rs
@@ -1,8 +1,6 @@
-use parking_lot::deadlock;
 use std::thread;
 use std::time::Duration;
 
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
